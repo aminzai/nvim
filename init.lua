2a98bc39d2820999260ce42057d1daa553ad6966
Amin Wang <lagunawang@gmail.com>
2023-03-28 14:35:22 +0800

    Use lsp-zero instead of coq+lsp
    Signed-off-by: Amin Wang <lagunawang@gmail.com>

--- init.lua
+++ init.lua
@@ -198,17 +198,37 @@ require("lazy").setup(
             end,
         },
         {
-            'ms-jpq/coq_nvim',
-            branch = 'coq',
+            'VonHeikemen/lsp-zero.nvim',
+            branch = 'v1.x',
             dependencies = {
-                {'ms-jpq/coq.artifacts', branch = 'artifacts'},
-                {'ms-jpq/coq.thirdparty', branch = '3p'},
+                -- LSP Support
+                {'neovim/nvim-lspconfig'},             -- Required
+                {'williamboman/mason.nvim'},           -- Optional
+                {'williamboman/mason-lspconfig.nvim'}, -- Optional
+                -- Autocompletion
+                {'hrsh7th/nvim-cmp'},         -- Required
+                {'hrsh7th/cmp-nvim-lsp'},     -- Required
+                {'hrsh7th/cmp-buffer'},       -- Optional
+                {'hrsh7th/cmp-path'},         -- Optional
+                {'saadparwaiz1/cmp_luasnip'}, -- Optional
+                {'hrsh7th/cmp-nvim-lua'},     -- Optional
+                -- Snippets
+                {'L3MON4D3/LuaSnip'},             -- Required
+                {'rafamadriz/friendly-snippets'}, -- Optional
             },
-            config = function()
-                --vim.cmd([[COQdeps]])
-                local coq = require("coq")
-                vim.cmd('COQnow -s')
+            config = function() 
+                local lsp = require('lsp-zero').preset({
+                    name = 'minimal',
+                    set_lsp_keymaps = true,
+                    manage_nvim_cmp = true,
+                    suggest_lsp_servers = false,
+                })
+                -- (Optional) Configure lua language server for neovim
+                lsp.nvim_workspace()
+                lsp.setup()
             end,
+
+
         },
         {
             "nvim-treesitter/nvim-treesitter",
